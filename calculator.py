529286968200a560d573d2f213146c38cc710b27
Maddie Smith <smitmad9@oregonstate.edu>
2021-02-04 08:43:30 -0800

    Implement arithmetic functions (+-*/)

---
+++ calculator.py
@@ -0,0 +1,16 @@
+# 1. Push calculator & tests to github repo & submit repo link
+# 2. Search for another unit testing framework - what is it and could
+#       you test calculator app using it? What are the differences? (pdf)
+
+
+def addition(x, y):
+    return x + y
+
+def subtraction(x, y):
+    return x - y
+
+def multiplication(x, y):
+    return x * y
+
+def division(x, y):
+    return float(x) / float(y)
